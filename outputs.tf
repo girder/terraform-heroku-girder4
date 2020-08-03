7c8af2690b50a241fb545bf63b99b6711fb2bebb
Brian Helba <brian.helba@kitware.com>
2020-08-03 15:13:34 -0400

    Return the storage bucket

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
-output "heroku_app_id" {
-  value       = module.heroku.app_id
-  description = "The Heroku app ID."
+output "heroku_app" {
+  value       = module.heroku.app
+  description = "The Heroku app."
 }
 
 output "iam_user" {
   value       = aws_iam_user.heroku_user
   description = "The IAM user for Heroku."
+}
+
+output "storage_bucket" {
+  value       = module.storage.bucket
+  description = "The storage S3 bucket."
 }
