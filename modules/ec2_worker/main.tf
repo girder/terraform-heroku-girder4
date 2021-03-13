6f857ae5084b280efeee8ed0a822b23b24243cbd
Brian Helba <brian.helba@kitware.com>
2021-03-13 13:23:21 -0500

    Use Ubuntu 20.04 for ec2_worker instances

--- modules/ec2_worker/main.tf
+++ modules/ec2_worker/main.tf
@@ -38,7 +38,7 @@ data "aws_ami" "ec2_worker" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 }
 
